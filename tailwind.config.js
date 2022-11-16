60e33daeb292aa5de618608763bcf398fb102bcc
Tasnim Prapty <79708581+tasnim2512@users.noreply.github.com>
2022-11-16 18:29:25 +0600

    Add files via upload
    ---> The information is displayed using an API. An API provides a fast, consistent and reliable way to get third party data. An API may be used for database communication between web applications. By extracting the implementation and relinquishing data into objects, an API simplifies programming exclamation
    
    ----> The API used here : (https://openweathermap.org/api)

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily:{
+        nunito: ['Nunito']
+      },
+      colors:{
+        darkblue:['#1F45FC']
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
